c9ea375eaaaebbe233351a86334702554d823be6
Fabian Kägy <mail@fabian-kaegy.de>
2021-01-24 17:19:55 +0100

    document Card class

--- gamelogic/Card.js
+++ gamelogic/Card.js
@@ -1,18 +1,35 @@
 /**
  * Card
- * color {colors}
- * number { 1-13 | N | Z }
  */
 class Card {
+
+	/**
+	 * Creates an instance of Card.
+	 * @param {*} color
+	 * @param {*} number
+	 * @memberof Card
+	 */
 	constructor(color, number) {
 		this.color = color;
 		this.value = number;
 	}
 
+	/**
+	 * get name
+	 *
+	 * @readonly
+	 * @memberof Card
+	 */
 	get name() {
 		return `${this.color} ${this.value}`;
 	}
 
+	/**
+	 * get sign
+	 *
+	 * @readonly
+	 * @memberof Card
+	 */
 	get sign() {
 		return this.value.sign;
 	}
