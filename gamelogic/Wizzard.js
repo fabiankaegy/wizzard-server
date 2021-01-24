579ad288c4be08dd31e6e752f993d1df8cdf34c0
Fabian Kägy <mail@fabian-kaegy.de>
2021-01-24 16:40:31 +0100

    add logic to start and end Round

--- gamelogic/Wizzard.js
+++ gamelogic/Wizzard.js
@@ -1,9 +1,12 @@
 const Player = require("./Player");
 const Deck = require("./Deck");
 const compareCards = require("./compareCards");
+const { shuffle } = require('./helper');
 
 /**
  * Wizzard
+ * 
+ * This is the main game implementation of the game Wizzard
  */
 class Wizzard {
 	constructor(Players) {
@@ -12,60 +15,115 @@ class Wizzard {
 		this.rounds = this.deck.cards.length / this.players.length;
 		this.currentRound = 1;
 		this.currentSubRound = 1;
-		this.currentTrump = undefined;
+		this.currentTrump = null;
+
+		this.startGame();
 	}
 
+
+	/**
+	 * startGame
+	 * 
+	 * initialize a new game and start the first round
+	 *
+	 * @memberof Wizzard
+	 */
+	startGame() {
+		this.setRandomPlayerOrder();
+		this.startRound();
+	}
+
+
+	/**
+	 * movePlayerIndex
+	 * 
+	 * move the first player in the player array to the 
+	 * end and therefore bring everone else one step closer
+	 * to the start. The order of the array determines the 
+	 * order in which the game gets played.
+	 *
+	 * @memberof Wizzard
+	 */
 	movePlayerIndex() {
 		const first = this.players.shift();
 		this.players.push(first);
 	}
 
+	
+	/**
+	 * startRound
+	 * 
+	 * starts a new round of the game
+	 *
+	 * @memberof Wizzard
+	 */
 	startRound() {
-		this.subrounds = this.currentRound;
-		this.deck = new Deck();
 		this.deck.reset();
 		this.deck.shuffle();
-		this.players.forEach(player => {
-			player.drawCards(this.deck.drawCards(this.currentRound));
-		});
+		this.drawCards();
+		this.drawTrumpf();
+	}
+
+	/**
+	 * endRound
+	 * 
+	 * end the currentRound and either end the game or start
+	 * a new round.
+	 *
+	 * @memberof Wizzard
+	 */
+	endRound() {
+		if ( this.currentRound === this.rounds ) {
+			// the game is finished now. Show the Winner. 
+		} else {
+			this.movePlayerIndex();
+			this.currentRound++;
+			this.startRound();
+		}
+	}
+
+	/**
+	 * setRandomPlayerOrder
+	 * 
+	 * shuffle arround the array of players to randomly 
+	 * choose the order in which the rounds will be played.
+	 * In a normal game this will only be called once as 
+	 * part of the initial setup. 
+	 *
+	 * @memberof Wizzard
+	 */
+	setRandomPlayerOrder() {
+		this.players = shuffle( this.players );
+	}
+
+
+	/**
+	 * drawCards
+	 * 
+	 * Have each player draw their cards at the beginning
+	 * of each round.
+	 *
+	 * @memberof Wizzard
+	 */
+	drawCards() {
+		this.players.forEach( player => {
+			player.setCards( this.deck.drawCards( this.currentRound ) )
+		} )
+	}
+
+	drawTrumpf() {
 		this.currentTrump = this.deck.drawCards(1)[0];
 	}
-
-	endRound() {
-		this.movePlayerIndex();
-		this.currentRound++;
-	}
-
-	startSubRound() {}
-
-	playCard(currentCard, player) {
-		if (
-			!this.lastBestCard.card ||
-			!compareCards(this.lastBestCard.card, currentCard)
-		) {
-			this.lastBestCard = { card: currentCard, player };
-		}
-
-		if (this.subrounds === this.currentSubRound) {
-			player.subroundsWon++;
-		}
-	}
-
-	get score() {
-		return this.players.sort((a, b) => a.score < b.score);
-	}
-
-	get player() {
-		return this.players.length;
-	}
-
-	get currentPlayer() {
-		return this.players[0];
-	}
-
-	findPlayer(id) {
-		return this.players.filter(player => player.id === id)[0];
-	}
+	
 }
 
 module.exports = Wizzard;
+
+
+const myGame = new Wizzard( [ new Player('Fabian'), new Player('Bea'), new Player('Tim'), new Player('moritz') ] );
+
+myGame.currentRound = 15;
+myGame.startRound();
+myGame.players[0].cards //?
+
+myGame.currentTrump //?
