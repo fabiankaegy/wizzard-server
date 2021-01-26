471dfcdd7e02502e9ab051ae264a900d2a38abf9
Fabian Kägy <mail@fabian-kaegy.de>
2021-01-26 21:45:19 +0100

    update types

--- gamelogic/Types.ts
+++ gamelogic/Types.ts
@@ -1,25 +1,40 @@
 import Player from './Player';
 import Card from './Card';
 
-export enum SocketInteractions {
+enum SocketInteractions {
     recivePlayerInteraction = 'player_interaction',
+    startGame = 'start_game',
+    welcome = 'welcome',
     shareGameState = 'share_game_state',
-    sharePlayerState = 'share_player_state',
+	sharePlayerState = 'share_player_state', 
+	createPlayer = 'create_player',
+	playerCreated = 'player_created', // used to share the private player info before the game started
+	sharePlayersList = 'share_players_list', // used to share the list of players public info before the game started
 };
 
-export enum PlayerInteractions {
+type SocketInteraction = keyof SocketInteractions;
+
+enum PlayerInteractions {
 	recivePrediction,
 	reciveMove
 };
 
-export enum GameState {
+enum GameState {
 	Created,
 	Started,
 	InProgress,
 	Completed
 };
 
-export interface PlayerCardCombo {
+interface PlayerCardCombo {
     player: Player,
     card: Card,
+}
+
+export {
+	SocketInteractions,
+	SocketInteraction,
+	PlayerInteractions,
+	GameState,
+	PlayerCardCombo
 }
--- server.ts
+++ server.ts
@@ -1,48 +1,45 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import http from "http";
-import socketio from 'socket.io';
+import socketio, { Socket } from 'socket.io';
+import path from 'path';
+
+import Wizzard from "./gamelogic/Wizzard";
+import Player from "./gamelogic/Player";
+import { SocketInteractions } from "./gamelogic/Types";
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-import Wizzard from "./gamelogic/Wizzard";
-import Player from "./gamelogic/Player";
-
-app.get("/", function(req: Request, res: Response) {
-	res.json({ message: "Hello World!" });
-});
-
 let players: Player[] = [];
 let game: Wizzard;
 
 io.sockets.on("connection", socket => {
-	socket.emit("welcome", {
+	socket.emit( SocketInteractions.welcome , {
 		players
 	});
 
-	socket.on("add_player", player => {
+	socket.on(SocketInteractions.createPlayer, player => {
 		const newPlayer = new Player(player.name, socket.id, socket);
 		players.push(newPlayer);
-		socket.emit("player_created", newPlayer.privateInfo);
+		socket.emit( SocketInteractions.playerCreated, newPlayer.privateInfo);
 		io.emit(
-			"player_added",
+			SocketInteractions.sharePlayersList,
 			players.map(player => player.publicInfo)
 		);
 	});
 
-	socket.on("start_game", () => {
+	socket.on( SocketInteractions.startGame, () => {
 		console.log("Game Starting");
 		game = new Wizzard(players, io );
 	});
 });
 
-io.sockets.on( 'reconnect', () => {
-	if ( game ) {
-		game.shareGameState();
-	}
-} )
+// Handles any requests that don't match the ones above
+app.get('*', express.static( path.join(__dirname+'/client/build/')) );
 
-server.listen(3000, function() {
-	console.log("listening on *:3000");
+const port = process.env.PORT || 5000;
+
+server.listen(port, function() {
+	console.log(`App is listening on port ${port}`);
 });
